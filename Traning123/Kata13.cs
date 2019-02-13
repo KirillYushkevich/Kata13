bc3b0b0ae987f858d68a4da70fd99d2dd3aa33af
KirillYushkevich <47444317+KirillYushkevich@users.noreply.github.com>
2019-02-14 02:52:23 +0300

    Solution

---
+++ Traning123/Kata13.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traning123
+{
+    public static class Kata
+    {
+        public static string addOrChangeUrlParameter(string url, string param)
+        {
+            string[] key = param.Split('=');
+            if (url.IndexOf(key[0]) == -1)
+            {
+                if(url.IndexOf("key")==-1)return url + "?" + param;
+                else return url + "&" + param;
+            }
+            else
+            {
+                int index = url.IndexOf(key[0]);
+                int indexConcat = url.IndexOf('&');
+                if (indexConcat == -1)
+                {
+                    string temp = url.Remove(index);
+                    return temp + param;
+                }
+                else
+                {
+                    string temp = url.Remove(index, indexConcat);
+                    temp = temp.Insert(index, param);
+                    return temp;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Traning123/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Traning123
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void TestSimple()
+        {
+            Assert.AreEqual(Kata.addOrChangeUrlParameter("www.example.com", "key=value"), "www.example.com?key=value");
+        }
+        [Test]
+        public void TestRemove()
+        {
+            Assert.AreEqual(Kata.addOrChangeUrlParameter("www.example.com?key=value", "key2=value2"), "www.example.com?key=value&key2=value2");
+        }
+        [Test]
+        public void TestReplace()
+        {
+            Assert.AreEqual(Kata.addOrChangeUrlParameter("www.example.com?key=oldValue`", "key=newValue"), "www.example.com?key=newValue");
+        }
+    }
+}
